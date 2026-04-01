f6ca008e443fc3aed44faffd827e702103395f32
Daniel Ferral <love00metal@gmail.com>
2026-04-01 15:31:50 -0600

    token interceptor

--- src/app/interceptors/loading-interceptor.ts
+++ src/app/interceptors/loading-interceptor.ts
@@ -16,6 +16,19 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+
+  const token = localStorage.getItem('access_token'); // o desde MSAL
+  console.log('Token en interceptor:', token);
+  
+  if (token) {
+    const cloned = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`)
+    });
+    return next.handle(cloned);
+  }
+  return next.handle(req);
+
+
     console.log('Interceptor ejecutado:', req.url);
     console.log('REQ:', req.url);
     console.log('HEADERS:', req.headers);
