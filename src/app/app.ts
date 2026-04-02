3d45418fa4250cdaef1932db39df92f08484b859
Daniel Ferral <love00metal@gmail.com>
2026-04-01 18:00:47 -0600

    user credential

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EstadosComponent } from "./components/estado.component/estado.component";
 import { ListBalancesModule } from './modules/list-balances/list-balances-module';
@@ -6,6 +6,9 @@ import { PdfComponent } from "./components/pdf.component/pdf.component";
 import { SincronizarComponent } from "./components/sincronizar.component/sincronizar.component";
 import { NgIf } from '@angular/common';
 import { LoaderComponent } from './components/loader.component/loader.component';
+import { AccountInfo } from '@azure/msal-browser';
+import { MsalService } from '@azure/msal-angular';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -14,16 +17,88 @@ import { LoaderComponent } from './components/loader.component/loader.component'
   styleUrl: './app.scss',
   template: `<msal-redirect></msal-redirect>`,
 })
-export class App {
+export class App implements OnInit {
+  account: AccountInfo | null = null;
+
+  /**
+   *
+   */
+  constructor(private msalService: MsalService, private authService: AuthService) {
+  
+  }
+  
+  userPhotoUrl: string | null = null;
+
+  cargarFoto() {
+    this.authService.getUserPhoto().subscribe({
+      next: (blob) => {
+        this.userPhotoUrl = URL.createObjectURL(blob);
+      },
+      error: () => {
+        this.userPhotoUrl = null; // fallback
+      }
+    });
+  }
+
+  ngOnInit(): void {
+     this.account = this.msalService.instance.getActiveAccount();
+
+      if (!this.account) {
+        const accounts = this.msalService.instance.getAllAccounts();
+        if (accounts.length > 0) {
+          this.account = accounts[0];
+          this.msalService.instance.setActiveAccount(this.account);
+        }
+      }
+
+      this.userName = this.account?.name || '';
+      this.userEmail = this.account?.username || '';
+
+      this.cargarFoto();
+  }
+
   protected readonly title = signal('CuentaFrontend');
 
   panelActivo: string = 'inicio';
+  menuAbierto = false;
+
+  userName = '';
+  userEmail = '';
 
   mostrar(panel: string) {
     this.panelActivo = panel;
+    this.obtenerUsuario();
+  }
+
+  obtenerUsuario() {
+    const token = localStorage.getItem('access_token');
+
+    if (!token) return;
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+
+    this.userName = payload.name;
+    this.userEmail = payload.preferred_username;
   }
 
   actualizarPropiedad(valor: string) {
     this.panelActivo = valor;
+  }
+
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  logout(event: Event) {
+    event.stopPropagation();
+
+    this.msalService.logoutRedirect({
+      postLogoutRedirectUri: window.location.origin
+    });
+  }
+
+  @HostListener('document:click')
+  clickFuera() {
+    this.menuAbierto = false;
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  constructor(private msal: MsalService) {}
+  constructor(private msal: MsalService, private http: HttpClient) {}
 
   login() {
     this.msal.loginRedirect();
@@ -17,4 +19,11 @@ export class AuthService {
   getUser() {
     return this.msal.instance.getAllAccounts()[0];
   }
+
+  getUserPhoto(): Observable<Blob> {
+  return this.http.get(
+    'https://graph.microsoft.com/v1.0/me/photo/$value',
+    { responseType: 'blob' }
+  );
+}
 }
